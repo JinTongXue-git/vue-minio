881d095b172d22679912cc1188314c6b1909f741
JinTongXue-git <coc3209265610@gmail.com>
2026-05-28 10:03:13 +0800

    m3 refactor: 调整项目路由与导入路径，新增表格页面与axios封装
    1. 修正App.vue的导入路径问题，将@/App.vue改为./App.vue
    2. 注释掉默认的home和about路由，移除默认路由组件导入
    3. 重写App.vue，替换为Element Plus表格组件并添加示例数据
    4. 新增axios请求封装类Request.js，实现单例模式与常用请求方法封装

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+
+/*测试数据*/
+const tableData = ref([
+  {
+    date: '2016-05-02',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1518 弄'
+  },
+  {
+    date: '2016-05-04',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1517 弄'
+  },
+  {
+    date: '2016-05-01',
+    name: '王小虎',
+    address: '上海市普陀区金沙江路 1519 弄'
+  }
+])
 </script>
 
+<!-- template标签里面写html代码 -->
 <template>
-  
+  <el-table :data="tableData" stripe style="width: 80%; margin: 0 auto;">
+    <el-table-column prop="date" label="Date" width="180" />
+    <el-table-column prop="name" label="Name" width="180" />
+    <el-table-column prop="address" label="Address" />
+  </el-table>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
---
+++ src/http/Request.js
@@ -0,0 +1,108 @@
+
+import axios from 'axios'
+
+class Request {
+
+  static instance = null
+
+  constructor(baseURL){
+    this.instance = axios.create({
+      baseURL: baseURL,
+      timeout: 5000
+    })
+    this.setupInterceptors()
+  }
+  // 获取实例 (单例模式)
+  static getInstance(baseURL){
+    if(Request.instance === null){
+      Request.instance = new Request(baseURL)
+    }
+    return Request.instance
+  }
+
+  // 设置拦截器
+  setupInterceptors(){
+    this.instance.interceptors.request.use((config) => {
+      return config
+    }),
+    (error) => {
+      return Promise.reject(error)
+    }
+
+    this.instance.interceptors.response.use(
+      (response) => {
+        return response
+      },
+      (error) => {
+        return Promise.reject(error)
+      }
+    )
+  }
+
+  // 封装常用请求方法
+
+  /**
+   * GET 请求
+   * @param {string} url - 请求地址
+   * @param {object} config - 请求配置
+   * @returns {Promise}
+   * @example
+   * // 获取用户列表
+   * request.get('/users').then(res => console.log(res))
+   * // 带参数的 GET 请求
+   * request.get('/users', { params: { page: 1, size: 10 } })
+   */
+  get(url, config = {}) {
+    return this.instance.get(url, config)
+  }
+
+  /**
+   * POST 请求
+   * @param {string} url - 请求地址
+   * @param {object} data - 请求体数据
+   * @param {object} config - 请求配置
+   * @returns {Promise}
+   * @example
+   * // 创建用户
+   * request.post('/users', { name: '张三', age: 25 })
+   * // 提交表单数据
+   * request.post('/login', { username: 'admin', password: '123456' })
+   */
+  post(url, data = {}, config = {}) {
+    return this.instance.post(url, data, config)
+  }
+
+  /**
+   * PUT 请求
+   * @param {string} url - 请求地址
+   * @param {object} data - 更新数据
+   * @param {object} config - 请求配置
+   * @returns {Promise}
+   * @example
+   * // 更新用户信息
+   * request.put('/users/1', { name: '李四', age: 26 })
+   * // 批量更新
+   * request.put('/users/batch', [/* 用户数组 ])
+   */
+  put(url, data = {}, config = {}) {
+    return this.instance.put(url, data, config)
+  }
+
+  /**
+   * DELETE 请求
+   * @param {string} url - 请求地址
+   * @param {object} config - 请求配置
+   * @returns {Promise}
+   * @example
+   * // 删除单个用户
+   * request.delete('/users/1')
+   * // 批量删除
+   * request.delete('/users', { params: { ids: [1, 2, 3] } })
+   */
+  delete(url, config = {}) {
+    return this.instance.delete(url, config)
+  }
+  
+}
+const request = Request.getInstance('http://192.168.100.129:9000')
+export default request
